94d0d1ff0b3582174ea8ed64dbe0f453b8994d6c
KodeWerk <brandonawilliams1@gmail.com>
2023-04-01 21:47:03 -0500

    finished helper function, created event listner to take in events passed from helper

--- js/app.js
+++ js/app.js
@@ -1,19 +1,4 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
 */
@@ -23,15 +8,36 @@
  * 
 */
 
-
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
+// a helper function when called will add an Li elemeent to the ul in the navbar
+function navbarHelper(numItems){
+    const queryNav = document.querySelector('.navbar__menu');
+    const queryNavUl = document.querySelector('#navbar__list');
+    
+    // repeat create Navbar Li element 3x
+    for (let i = 1; i <= numItems; i++) {
+        const navLiEl = document.createElement('li');
+        navLiEl.style.color = 'black';
+        // navLiEl.textContent = "Item " + (i);
+        navLiEl.innerHTML = `<a href='#section${i}' id='navItem ${i}' class= 'menu__link'>Section ${i}</a>`
+        queryNavUl.appendChild(navLiEl);
 
+        navLiEl.addEventListener('click', navTester)
+    };
+    
+    queryNav.appendChild(queryNavUl);
+    // const createNavLi = navLiEl.textContent= "Section 1";
+    return queryNav;
+};
 
+function navTester(e){
+console.log(e.target.id)
+};
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -39,6 +45,7 @@
 */
 
 // build the nav
+const myNavBar = navbarHelper(3);
 
 
 // Add class 'active' to section when near top of viewport
