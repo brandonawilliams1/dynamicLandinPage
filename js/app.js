6aa624369badb2c4fdd8e627816e50d3de33fa01
KodeWerk <brandonawilliams1@gmail.com>
2023-04-13 09:28:48 -0500

    remove comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
 
 /**
  * Define Global Variables
